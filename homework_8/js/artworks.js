5334ad3c968f19e4d8ea135268b1707029282bfc
Song Tang <songtang@cfa323-03.andrew.cmu.edu>
2021-12-02 22:20:05 -0500

    added project page interactions

--- homework_8/js/artworks.js
+++ homework_8/js/artworks.js
@@ -2,11 +2,11 @@ $(function() {
 		var selectedClass = "";
 		$(".fil-cat").click(function(){ 
 		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+     $("#artworks").fadeTo(100, 0.1);
+		$("#artworks div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
     setTimeout(function() {
       $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
+      $("#artworks").fadeTo(300, 1);
     }, 300); 
 		
 	});
