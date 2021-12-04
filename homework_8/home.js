e9823b164cd540784a33d5c21d524780c46520ce
Song Tang <songtang@cfa323-03.andrew.cmu.edu>
2021-12-03 20:55:43 -0500

    updatd paes

---
+++ homework_8/home.js
@@ -0,0 +1,7 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['project summary', 'domain research'],
+    // sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['1 page', '2 page']
+});
